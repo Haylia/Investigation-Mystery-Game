699c754e3b20b9883dc5c13babd88e2ade4460ca
haylia <karnichel@gmail.com>
2024-01-08 01:00:21 +0000

    omg its the house!!!!

--- Assets/Scripts/start.cs
+++ Assets/Scripts/start.cs
@@ -9,7 +9,21 @@ public class start : MonoBehaviour
     {
         GameObject.Find("ExplainCanvas").SetActive(false);
         GameObject.Find("ProtagMenu").SetActive(false);
-
+         GameObject.Find("Foyer Room Shade").SetActive(false);
+         // GameObject.Find("Parlour Room Shade").SetActive(false);
+         GameObject.Find("Dining Room Shade").SetActive(false);
+         GameObject.Find("Library Room Shade").SetActive(false);
+        GameObject.Find("Lounge Room Shade").SetActive(false);
+        GameObject.Find("Pantry Room Shade").SetActive(false);
+         GameObject.Find("Kitchen Room Shade").SetActive(false);
+        GameObject.Find("Chefs Room Shade").SetActive(false);
+        GameObject.Find("Maids Room Shade").SetActive(false);
+        GameObject.Find("Butlers Room Shade").SetActive(false);
+         GameObject.Find("Child Room Shade").SetActive(false);
+        GameObject.Find("Host Room Shade").SetActive(false);
+         GameObject.Find("Guest 1 Room Shade").SetActive(false);
+         GameObject.Find("Study Room Shade").SetActive(false);
+         GameObject.Find("Guest 2 Room Shade").SetActive(false);
         gameObject.SetActive(false);
     }
 
